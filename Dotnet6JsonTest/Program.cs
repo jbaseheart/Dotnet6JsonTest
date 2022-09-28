04318d5f1841a9040b455592369b755a9c8e89ea
Jonathan Baseheart <jonathanbaseheart@northwesternmutual.com>
2022-09-28 14:24:54 -0500

    Add fixes to have parity with .Net Framework 4.8 behavior

--- Dotnet6JsonTest/Program.cs
+++ Dotnet6JsonTest/Program.cs
@@ -3,7 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+.AddNewtonsoftJson(c => 
+{ 
+    c.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+    c.SerializerSettings.DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Include;    
+    c.SerializerSettings.Error = (sender, args) =>
+    {
+        var input = args.ErrorContext.OriginalObject;
+        // "input.Gender" will default to 0 (the default value)
+
+        args.ErrorContext.Handled = true;
+    };
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Dotnet6JsonTest/TestInput.cs
+++ Dotnet6JsonTest/TestInput.cs
@@ -1,17 +1,59 @@
-﻿namespace Dotnet6JsonTest
+﻿using Newtonsoft.Json;
+using System.Linq;
+
+namespace Dotnet6JsonTest
 {
     public class TestInput
     {
         public string Name { get; set; }
+
+        [JsonConverter(typeof(CustomEnumConverter))]
         public GenderType Gender { get; set; }
 
         public int Age { get; set; }
     }
 
-
+    
     public enum GenderType
     {
         Male,
         Female
+    }
+
+    public class CustomEnumConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType.IsEnum)
+            {
+                if (reader.TokenType == JsonToken.Integer && reader.ValueType == typeof(long))
+                {
+                    if (Enum.GetValues(objectType).Cast<int>().Select(e => (long)e).Contains((long)reader.Value))
+                        return Convert.ToInt32(reader.Value);
+                }
+                if (reader.TokenType == JsonToken.Integer && reader.ValueType == typeof(int))
+                {
+                    if (Enum.GetValues(objectType).Cast<int>().Contains((int)reader.Value))
+                        return reader.Value;
+                }
+                else if (reader.TokenType == JsonToken.String)
+                {
+                    if (Enum.GetNames(objectType).Contains((string)reader.Value))
+                        return Enum.Parse(objectType, (string)reader.Value);
+                }
+            }
+
+            return existingValue;
+        }
+        public override bool CanWrite => false;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
